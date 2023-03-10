69aa23a3f3da89c83ec6de14da8f118599fe69bd
David Chen <dc4472@nyu.edu>
2023-03-10 11:33:53 -0500

    Created the edit-task screen with route in App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import Home from './Home'
 import Header from './Header'
 import Menu from './Menu'
 import NewTask from './NewTask'
+import EditTask from './EditTask'
 
 
 const App = props => {
@@ -19,6 +20,7 @@ const App = props => {
             {/* a route to see a list of all messages */}
             <Route path="/menu" element={<Menu />} />
             <Route path="/newtask" element={<NewTask />} />
+            <Route path="/edittask" element={<EditTask />} />
             
 
           </Routes>
