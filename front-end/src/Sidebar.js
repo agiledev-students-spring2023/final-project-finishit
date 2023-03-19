b79f735539b79a330cebb90890e53c56018355d8
Bast1onic <al6253@nyu.edu>
2023-03-19 13:54:46 -0400

    Improve button functionality

--- front-end/src/Sidebar.js
+++ front-end/src/Sidebar.js
@@ -7,13 +7,12 @@ import './Sidebar.css'
  * @param {*} param0 an object holding any props passed to this component from its parent component
  * @returns The contents of this component, in JSX form.
  */
-//customBurgerIcon={<img src={menu_icon} alt="Hamburger"/>}
 const Sidebar = props => {
   return (
     <Menu className='bm-item-list' >
-      <Link to="/login" className='bm-item'><h4>Log In</h4></Link>
-      <Link to="/" className='bm-item'><h4>Main</h4></Link>
-      <Link to="/settings" className='bm-item'><h4>Settings</h4></Link>
+      <Link to="/login" className='bm-item' onClick={() => props.isOpen=false}><h4>Log In</h4></Link>
+      <Link to="/" className='bm-item' onClick={() => props.isOpen=false}><h4>Main</h4></Link>
+      <Link to="/settings" className='bm-item' onClick={() => props.isOpen=false}><h4>Settings</h4></Link>
     </Menu>
     
   );
