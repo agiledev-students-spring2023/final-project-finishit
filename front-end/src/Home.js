de5d5f7fcc213a63755c19b5381bfc6e8e5ad098
Harrison Douglass <hpdouglass10@gmail.com>
2023-03-19 13:07:44 -0400

    feat: Add ability to add badges in the task JSON

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -11,16 +11,16 @@ const daysAgo = days => {
 
 const Home = props => {
     const dummyTasks = [
-        { id: 1, title: 'Task 1', dueDate: daysAgo(-1), status: 'NOT_STARTED' },
-        { id: 2, title: 'Task 2', dueDate: daysAgo(0), status: 'IN_PROGRESS' },
-        { id: 3, title: 'Task 3', dueDate: daysAgo(1), status: 'COMPLETED' },
-        { id: 4, title: 'Task 4', dueDate: daysAgo(2), status: 'NOT_STARTED' },
-        { id: 5, title: 'Task 5', dueDate: daysAgo(3), status: 'IN_PROGRESS' },
-        { id: 6, title: 'Task 6', dueDate: daysAgo(4), status: 'COMPLETED' },
-        { id: 7, title: 'Task 7', dueDate: daysAgo(5), status: 'NOT_STARTED' },
-        { id: 8, title: 'Task 8', dueDate: daysAgo(6), status: 'IN_PROGRESS' },
-        { id: 9, title: 'Task 9', dueDate: daysAgo(7), status: 'COMPLETED' },
-        { id: 10, title: 'Task 10', dueDate: daysAgo(8), status: 'NOT_STARTED' },
+        { id: 1, title: 'Task 1', dueDate: daysAgo(-1), status: 'NOT_STARTED', badges: [{color: '#000000', text: 'Category 1'}, {color: '#ff0000', text: 'Category 3'}] },
+        { id: 2, title: 'Task 2', dueDate: daysAgo(0), status: 'IN_PROGRESS', badges: [{color: '#ffffff', text: 'Category 2'}] },
+        { id: 3, title: 'Task 3', dueDate: daysAgo(1), status: 'COMPLETED', badges: [{color: '#ccff99', text: 'Category 4'}] },
+        { id: 4, title: 'Task 4', dueDate: daysAgo(2), status: 'NOT_STARTED', badges: [{color: '#000000', text: 'Category 1'}] },
+        { id: 5, title: 'Task 5', dueDate: daysAgo(3), status: 'IN_PROGRESS', badges: [{color: '#ff0000', text: 'Category 3'}] },
+        { id: 6, title: 'Task 6', dueDate: daysAgo(4), status: 'COMPLETED', badges: [{color: '#000000', text: 'Category 1'}] },
+        { id: 7, title: 'Task 7', dueDate: daysAgo(5), status: 'NOT_STARTED', badges: [{color: '#ffffff', text: 'Category 2'}] },
+        { id: 8, title: 'Task 8', dueDate: daysAgo(6), status: 'IN_PROGRESS', badges: [{color: '#ffffff', text: 'Category 2'}] },
+        { id: 9, title: 'Task 9', dueDate: daysAgo(7), status: 'COMPLETED', badges: [{color: '#ffffff', text: 'Category 2'}] },
+        { id: 10, title: 'Task 10', dueDate: daysAgo(8), status: 'NOT_STARTED', badges: [{color: '#000000', text: 'Category 1'}] },
     ]
 
     const [tasks] = useState(dummyTasks)
@@ -29,11 +29,11 @@ const Home = props => {
 
     return (
         <div>
-            <div class="task-list">
+            <div className="task-list">
                 {filteredTasks.map((task, idx) => (
                     <React.Fragment key={idx}>
                         <Task key={task.id} task={task} />
-                        {idx < filteredTasks.length-1 && <hr class="task-separator" />}
+                        {idx < filteredTasks.length-1 && <hr className="task-separator" />}
                     </React.Fragment>
                 ))}
             </div>
