d780635c4697d99d62204cf6e323b7a76b1b0807
hpdouglass <hpdouglass10@gmail.com>
2023-03-19 12:38:53 -0400

    feat: Add nonresponsive task badges

--- front-end/src/Task.js
+++ front-end/src/Task.js
@@ -11,6 +11,9 @@ const Task = props => {
             <div class="task-info">
                 <div class="task-title">{title}</div>
                 <div class="task-due-date">{dueDate.toLocaleString()}</div>
+                <div class="task-categories">
+                    <span class="badge" style={{color: 'white', background: 'black'} /* todo: change style per category */}>Test 1</span>
+                </div>
             </div>
             <div class="task-checkbox">
                 <input type="checkbox" class="task-checkbox-input" checked={status === "COMPLETED"} />
