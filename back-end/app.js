64ff85b91f16ad3eea475ce11e2d8eb260eec544
dc4472 <dc4472@nyu.edu>
2023-03-29 14:50:44 -0400

    linting edits and 2s deletion on my end.

--- back-end/app.js
+++ back-end/app.js
@@ -1,26 +1,18 @@
 import express from 'express'
-import axios from 'axios'
 
 const app = express()
-
 app.get('/', (req, res) => {
     res.send('Hello!')
 })
 
-app.post("/newtask", async(req, res)=>{ 
-    
+/*
+app.post('/newtask', async(req, res) => {
+    name:req.body
 })
 
-app.post("/edittask", async(req, res)=>{ 
-    
-})
+app.post('/edittask', async(req, res) => {
 
-app.post("/newtask", async(req, res)=>{ 
-    
-})
-
-app.post("/edittask", async(req, res)=>{ 
-    
 })
 
 export default app
+*/
--- front-end/src/NewTask.js
+++ front-end/src/NewTask.js
@@ -1,51 +1,40 @@
 import './NewTask.css'
-import React, {useRef, useState} from 'react'
-import { Link } from 'react-router-dom';
-import axios from 'axios'
+import React, { useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+// import axios from 'axios'
 
-
-const NewTask = (props) => {
-
-    const [date, setDate] = useState('');
-    const dateInputRef = useRef(null);
-
-    const handleChange = (e) => {
-
-        setDate(e.target.value);
-
-
-    };
-
+const NewTask = props => {
+    const [date, setDate] = useState('')
+    const dateInputRef = useRef(null)
+    const handleChange = e => {
+        setDate(e.target.value)
+    }
     return (
         <>
             <h1>New Task</h1>
-
-            <form action='/' method='POST'>
+            <form action="/" method="POST">
                 <div>
-                    <label>Name of Task:</label><br />
-                    <input className="taskInputBox" type="text"></input>
+                    <label>Name of Task:</label>
+                    <br />
+                    <input className="taskInputBox" type="text" />
+                </div>
+                <div>
+                    <label>Reminder Date:</label>
+                    <br />
+                    <input type="date" onChange={handleChange} ref={dateInputRef} />
+                </div>
+                <div>
+                    <label>Due Date:</label>
+                    <br />
+                    <input type="date" onChange={handleChange} ref={dateInputRef} />
                 </div>
 
                 <div>
-                    <label >Reminder Date:</label><br />
-                    <input type="date" onChange={handleChange} ref={dateInputRef}/>
-                    
-                </div>
- 
-                <div>
-                    <label >Due Date:</label><br />
-                    <input type="date" onChange={handleChange} ref={dateInputRef}/>
-                </div>
-
-                <div>
-                    <Link to="/"><button className="submitButton">Submit Task</button></Link>
+                    <Link to="/"><button className="submitButton" type="submit">Submit Task</button></Link>
                 </div>
             </form>
-
         </>
-
     )
-
 }
 
-export default NewTask;
+export default NewTask
