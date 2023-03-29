9d86dd6eab48d71a7dfc87872bae48efe4d8bfd8
Bast1onic <al6253@nyu.edu>
2023-03-29 12:44:31 -0400

    Backend route for badge home

--- back-end/app.js
+++ back-end/app.js
@@ -1,8 +1,39 @@
+require('dotenv').config({ silent: true }) // load environmental variables from a hidden file named .env
 const express = require("express")
+const morgan = require('morgan')
+const cors = require('cors') // middleware for enabling CORS (Cross-Origin Resource Sharing) requests.
+
 const app = express()
+
+app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' }))
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("Hello!")
+})
+
+const sampleBadges = [
+    {color: '#000000', text: 'These'},
+    {color: '#ffffff', text: 'badges'},
+    {color: '#ff0000', text: 'are'},
+    {color: '#ccff99', text: 'from'},
+    {color: '#ff0000', text: 'the'},
+    {color: '#f5b942', text: 'backend'}
+]
+
+//transfers badge data from db to /badgehome on front-end
+app.get('/badges', async (req, res) => {
+    try {
+        res.json({
+          badges: sampleBadges
+        })
+      } catch (err) {
+        console.error(err)
+        res.status(400).json({
+          error: err,
+          status: 'failed to retrieve badges from the database',
+        })
+      }
 })
 
 module.exports = app
--- front-end/src/BadgeHome.js
+++ front-end/src/BadgeHome.js
@@ -1,6 +1,7 @@
 import './BadgeHome.css'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const BadgeHome = props => {
     const textColorFromBackground = background => {
@@ -13,6 +14,7 @@ const BadgeHome = props => {
         return (rgb[0]*0.299 + rgb[1]*0.587 + rgb[2]*0.114) > 154 ? 'black' : 'white'
     }
 
+    
     const sampleBadges = [
         {color: '#000000', text: 'Category 1'},
         {color: '#ffffff', text: 'Category 2'},
@@ -22,7 +24,34 @@ const BadgeHome = props => {
         {color: '#f5b942', text: 'Medium Priority'}
     ]
 
-    const [badges] = useState(sampleBadges)
+    const [badges, setBadges] = useState(sampleBadges)
+
+    const fetchBadges = () => {
+        //    env var in .env
+        axios.get(`${process.env.REACT_APP_SERVER_HOSTNAME}/badges`)//returns a promise
+      .then(response => {
+        setBadges(response.data.badges)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+
+    useEffect(() => {
+        // fetch badges this once
+        fetchBadges()
+    
+        // set a timer to load data from server every n seconds
+        const intervalHandle = setInterval(() => {
+          fetchBadges()
+        }, 5000)
+    
+        // return a function that will be called when this component unloads
+        return e => {
+          // clear the timer, so we don't still load badges when this component is not loaded anymore
+          clearInterval(intervalHandle)
+        }
+    }, [])
 
     return (
         <div className="pagebody">
