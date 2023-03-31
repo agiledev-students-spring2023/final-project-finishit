21b5554bd0a44464ccf4c852f31b97745814ec95
Bast1onic <al6253@nyu.edu>
2023-03-31 13:49:28 -0400

    Finish previous merge

--- back-end/app.mjs
+++ back-end/app.mjs
@@ -1,23 +1,21 @@
-// load environmental variables from a hidden file named .env
 import express from 'express'
-import * as dotenv from 'dotenv'
-import morgan from 'morgan'
-import cors from 'cors'
-
-// route imports
-import badgesRouter from './routes/badges.mjs'
-
-dotenv.config({ silent: true })
+// import axios from 'axios'
+// import cors from 'cors'
 
 const app = express()
-
-app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' }))
-app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello!')
 })
 
-app.use('/', badgesRouter)
+/*
+app.post('/newtask', async(req, res) => {
+    name:req.body
+})
+
+app.post('/edittask', async(req, res) => {
+
+})
 
 export default app
+*/
