f4a17e4c302c443f4cca3a9e8162929330feb02f
dc4472 <dc4472@nyu.edu>
2023-04-03 13:50:40 -0400

    route created

--- back-end/routes/NewTask.mjs
+++ back-end/routes/NewTask.mjs
@@ -1,18 +1,41 @@
-const express = require('express')
-const cors = require('cors')
+import express from 'express'
+// import tasksRouter from './tasks.mjs'
 
-const port = 5000
-const app = express()
+const newrouter = express.Router()
 
-app.use(cors())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+const daysAgo = days => {
+    const d = new Date()
+    d.setDate(d.getDate() + days)
+    return d
+}
 
-app.post('/', (req, res) => {
-    const { email, password } = req.body
-    console.log(`Your Email is ${email} and your password is ${password}`)
+const sampleTasks = [
+    { id: 1, title: 'Finish essay', status: 'NOT_STARTED', badges: [{ id: 1, color: '#ff5733', text: 'School' }, { id: 2, color: '#4f2f4f', text: 'Writing' }] },
+    { id: 2, title: 'Buy groceries', status: 'IN_PROGRESS', badges: [{ id: 3, color: '#00bfff', text: 'Shopping' }] }
+
+]
+
+newrouter.post('/newtask', async (req, res) => {
+    const task = req.body
+    console.log(sampleTasks)
+    console.log(task)
+    sampleTasks.push(task)
+    res.send('New task has been stored. Thank you!')
+    console.log(sampleTasks)
 })
 
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
+newrouter.get('/tasks', async (req, res) => {
+    try {
+        res.json({
+            tasks: sampleTasks
+        })
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({
+            error: err,
+            status: 'failed to retrieve tasks from the database'
+        })
+    }
 })
+
+export default newrouter
