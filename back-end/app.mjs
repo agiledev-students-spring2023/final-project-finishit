2c1e00397f6b9d325b11f61f2b7ff1c06b327296
dc4472 <dc4472@nyu.edu>
2023-04-09 17:59:27 -0400

    added my router to app.mjs backend

--- back-end/app.mjs
+++ back-end/app.mjs
@@ -7,6 +7,7 @@ import cors from 'cors'
 import badgesRouter from './routes/badges.mjs'
 import tasksRouter from './routes/tasks.mjs'
 import newrouter from './routes/NewTask.mjs'
+import editrouter from './routes/EditTask.mjs'
 
 const app = express()
 // use express's builtin body-parser middleware to parse any data included in a request
@@ -23,6 +24,7 @@ app.get('/', (req, res) => {
 app.use('/', badgesRouter)
 app.use('/', tasksRouter)
 app.use('/', newrouter)
+app.use('/', editrouter)
 
 /*
 app.post('/newtask', async(req, res) => {
