e4df21ffe0c6a9b2b8953062d6874a518a078125
Khalifa-AlFalasi <ksa330@nyu.edu>
2023-04-10 04:10:52 -0400

    added logout feature and usrrname display

--- front-end/src/Sidebar.js
+++ front-end/src/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { slide as Menu } from 'react-burger-menu'
 import './Sidebar.css'
@@ -8,15 +8,64 @@ import './Sidebar.css'
  * @param {*} param0 an object holding any props passed to this component from its parent component
  * @returns The contents of this component, in JSX form.
  */
-const Sidebar = props => (
-    <Menu className="bm-item-list">
-        <Link to="/login" className="bm-item" onClick={() => { props.isOpen = false }}><h4>Log In</h4></Link>
-        <Link to="/" className="bm-item" onClick={() => { props.isOpen = false }}><h4>Main</h4></Link>
-        <Link to="/badges" className="bm-item" onClick={() => { props.isOpen = false }}><h4>Badges</h4></Link>
-        <Link to="/settings" className="bm-item" onClick={() => { props.isOpen = false }}><h4>Settings</h4></Link>
-    </Menu>
+const Sidebar = props => {
+    const [user, setUser] = useState(null)
+    const logoutHandler = e => {
+        console.log('in logout handler')
+        e.preventDefault()
+        localStorage.clear()
+        window.location = '/login'
+    }
 
-)
+    useEffect(() => {
+        if (localStorage.getItem('token') !== undefined) {
+            console.log('logged in user', localStorage.getItem('user'))
+            setUser(JSON.parse(localStorage.getItem('user')))
+        }
+    }, [])
+
+    return (
+        <Menu className="bm-item-list">
+            {!user && (
+                <Link
+                    to="/login"
+                    className="bm-item"
+                    onClick={() => {
+                        props.isOpen = false
+                    }}
+                >
+                    <h4>Log In</h4>
+                </Link>
+            )}
+            {user && (
+                <Link
+                    to="/"
+                    className="bm-item"
+                    onClick={() => {
+                        props.isOpen = false
+                    }}
+                >
+                    <h5>{user.username}</h5>
+                </Link>
+            )}
+            {user && (
+                <Link
+                    to="/logout"
+                    className="bm-item"
+                    onClick={logoutHandler}
+                    style={{
+                        color: 'rebeccapurple'
+                    }}
+                >
+                    <h4>Logout</h4>
+                </Link>
+            )}
+            <Link to="/" className="bm-item" onClick={() => { props.isOpen = false }}><h4>Main</h4></Link>
+            <Link to="/badges" className="bm-item" onClick={() => { props.isOpen = false }}><h4>Badges</h4></Link>
+            <Link to="/settings" className="bm-item" onClick={() => { props.isOpen = false }}><h4>Settings</h4></Link>
+        </Menu>
+    )
+}
 
 // make this component available to be imported into any other file
 export default Sidebar
