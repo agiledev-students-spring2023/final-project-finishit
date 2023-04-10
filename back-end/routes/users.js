db5f414acc4f14994db7c4fc4865eab0c3cd7780
Bast1onic <al6253@nyu.edu>
2023-04-10 11:01:14 -0400

    Change everything to CommonJS syntax

---
+++ back-end/routes/users.js
@@ -0,0 +1,84 @@
+const express = require('express')
+// eslint-disable-next-line import/no-extraneous-dependencies
+const jwt = require('jsonwebtoken')
+const { promises } = require('fs')
+
+const fs = promises
+const { fileURLToPath } = require('url')
+const { path, dirname } = require('path')
+
+const usersRouter = express.Router()
+
+// eslint-disable-next-line no-underscore-dangle
+const __filename = fileURLToPath(import.meta.url)
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = dirname(__filename)
+
+usersRouter.post('/login', async (req, res) => {
+    // check if the user exists
+
+    const filePath = path.join(__dirname, '../data/users.json')
+
+    const usersInJSON = await fs.readFile(filePath, 'utf-8')
+
+    const users = JSON.parse(usersInJSON)
+    const found = users.find(user => user.username === req.body.username)
+    console.log(found)
+    if (!users.find(user => user.username === req.body.username)) {
+        res.status(400).json({
+            success: false,
+            message: 'Invalid credentials!'
+        })
+        return
+    }
+
+    // check of username and password both
+    const foundUser = users.find(user => user.username === req.body.username)
+    if (foundUser.password === req.body.password) {
+        // generate token
+        const token = jwt.sign(
+            { username: foundUser.username },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: process.env.JWT_EXPIRE
+            }
+        )
+
+        res.status(200).json({
+            success: true,
+            token
+        })
+    }
+})
+
+usersRouter.post('/create', async (req, res) => {
+    const filePath = path.join(__dirname, '../data/users.json')
+
+    const usersInJSON = await fs.readFile(filePath, 'utf-8')
+
+    const users = JSON.parse(usersInJSON)
+
+    const userExists = users.find(user => user.username === req.body.username)
+
+    if (userExists) {
+        res.status(400).json({
+            success: false,
+            message: 'Username already exists'
+        })
+        return
+    }
+
+    const newUsersList = [...users, req.body]
+
+    const response1 = await fs.writeFile(filePath, JSON.stringify(newUsersList))
+
+    // check of username and password both
+    res.status(200).json({
+        success: true,
+        message: 'account created successfully'
+    })
+})
+
+module.exports = {
+    default: usersRouter
+}
