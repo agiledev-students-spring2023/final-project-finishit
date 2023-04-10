41d8f9d085a248719731760e858e42f208ae2250
Khalifa-AlFalasi <ksa330@nyu.edu>
2023-04-09 21:19:13 -0400

    did backend for reset pages

--- back-end/routes/users.mjs
+++ back-end/routes/users.mjs
@@ -38,6 +38,39 @@ const saveUsers = async users => {
     return true
 }
 
+const getUsers = async () => {
+    const filePath = path.join(__dirname, '../data/users.json')
+    const usersInJSON = await fs.readFile(filePath, 'utf-8')
+    return JSON.parse(usersInJSON)
+}
+
+const verifyQuestions = async (username, answers) => {
+    const users = await getUsers()
+    let verified = false
+    try {
+        users.forEach(user => {
+            if (
+                user.petName === answers.petName
+                && user.motherName === answers.motherName
+                && user.username === username
+            ) {
+                verified = true
+                throw new Error('verified')
+            }
+        })
+    } catch (e) {
+        if (e.message !== 'verified') {
+            throw e
+        }
+    }
+    return verified
+}
+
+const findUserByUsername = async username => {
+    const users = await getUsers()
+    return users.find(user => user.username === username)
+}
+
 usersRouter.post('/login', async (req, res) => {
     // check if the user exists
 
@@ -165,6 +198,68 @@ usersRouter.put('/', async (req, res) => {
     }
 
     // check if user is loggd in
+})
+
+usersRouter.post('/verify-answers', async (req, res) => {
+    const { username, answers } = req.body
+    const found = await findUserByUsername(username)
+    if (!found) {
+        res.status(404).json({
+            success: false,
+            message: 'username not found!'
+        })
+        return
+    }
+
+    const verified = await verifyQuestions(username, answers)
+    if (!verified) {
+        res.status(403).json({
+            success: false,
+            message: 'You have answered wrong!'
+        })
+    } else {
+        res.status(200).json({
+            success: true,
+            message: 'verified!',
+            username
+        })
+    }
+})
+
+usersRouter.patch('/reset-password', async (req, res) => {
+    const { username, newPassword } = req.body
+    const found = await findUserByUsername(username)
+
+    if (!found) {
+        res.status(404).json({
+            success: false,
+            message: 'username not found!'
+        })
+        return
+    }
+
+    // reset Password
+
+    const users = await getUsers()
+    users.map(user => {
+        if (user.username === username) {
+            user.password = newPassword
+        }
+        return user
+    })
+
+    const response = await saveUsers(users)
+    if (!response) {
+        res.status(400).json({
+            success: false,
+            message: 'Password reset failed'
+        })
+    } else {
+        res.status(200).json({
+            success: true,
+            message: 'Password updated successfully'
+        })
+    }
 })
 
 export default usersRouter
