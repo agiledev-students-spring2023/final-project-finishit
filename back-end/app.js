dd61a1eb48ce334be5a9a70ef3caa38ce84dc8e0
hpdouglass <hpdouglass10@gmail.com>
2023-04-14 15:10:48 -0400

    feat: Add mongodb packages and connect to mongodb on startup

--- back-end/app.js
+++ back-end/app.js
@@ -1,34 +1,36 @@
+// NPM imports.
 const express = require('express')
-// eslint-disable-next-line import/no-extraneous-dependencies
 const dotenv = require('dotenv')
-// eslint-disable-next-line import/no-extraneous-dependencies
 const morgan = require('morgan')
-// eslint-disable-next-line import/no-extraneous-dependencies
 const cors = require('cors')
+const mongoose = require('mongoose')
 
-// route imports
+// Route imports.
 const badgesRouter = require('./routes/badges').badgesRouter
 const tasksRouter = require('./routes/tasks').tasksRouter
 const newrouter = require('./routes/NewTask').newrouter
 const usersRouter = require('./routes/users').usersRouter
 const editrouter = require('./routes/EditTask').editrouter
 
-// camel case
-
+// Initialize express app.
 const app = express()
-// use express's builtin body-parser middleware to parse any data included in a request
-app.use(express.json()) // decode JSON-formatted incoming POST data
-app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
+
+// Tell express to use certain middleware (imported above).
 dotenv.config({ silent: true })
 app.use(morgan('dev', { skip: (req, res) => process.env.NODE_ENV === 'test' }))
 app.use(cors())
-
-// middleware to parse JSON bodies
 app.use(express.json())
-
-// middleware to parse URL-encoded bodies
 app.use(express.urlencoded({ extended: true }))
 
+// Connect to MongoDB.
+try {
+    mongoose.connect(process.env.MONGODB_URI)
+    console.log('Connected to MongoDB.')
+} catch (err) {
+    console.log(`Error connecting to MongoDB: ${err}`)
+}
+
+// Specify routes (imported above).
 app.use('/', badgesRouter)
 app.use('/', tasksRouter)
 app.use('/', newrouter)
