e7aff31dbb7ec0ad8908859d592dd7212b869c75
Bast1onic <al6253@nyu.edu>
2023-04-22 21:51:35 -0400

    Use multiselect

--- front-end/src/NewTask.js
+++ front-end/src/NewTask.js
@@ -1,7 +1,8 @@
 import './NewTask.css'
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import Multiselect from 'multiselect-react-dropdown'
 
 const NewTask = props => {
     const jwtToken = localStorage.getItem('token')
@@ -17,6 +18,8 @@ const NewTask = props => {
     const [remdate, setremdate] = useState('')
     const [status, setstatus] = useState('')
     const [error, setError] = useState('')
+
+    const [options, setOptions] = useState([])
 
     const navigate = useNavigate()
 
@@ -39,6 +42,41 @@ const NewTask = props => {
             )
         })
     }
+
+    const handleSelect = (selectedList, selectedItem) => {
+
+    }
+
+    const handleRemove = (selectedList, selectedItem) => {
+
+    }
+
+    useEffect(() => {
+        if(!jwtToken) {
+            navigate('/login')
+        }else {
+            async function fetchBadges() {
+                try {
+                    const fetchedBadges = await axios.get(
+                        `${process.env.REACT_APP_SERVER_HOSTNAME}/badges`,
+                        { headers: { Authorization: `JWT ${jwtToken}` } }
+                    )
+                    if (fetchedBadges.data.status) {
+                        setError(fetchedBadges.data.status)
+                    } else {
+                        setOptions(fetchedBadges.data.badges.map(val => ({text: val.text, id: val._id.toString()})))
+                        setError('')
+                    }
+                } catch (err) {
+                    setError('Something went wrong when fetching badges. Please try again later.')
+                    console.log(err)
+                    // nav('/login')
+                }
+            }
+            fetchBadges()
+        }
+         
+    }, [jwtToken])
 
     return (
         <>
@@ -65,7 +103,12 @@ const NewTask = props => {
                     <br />
                     <input className="taskInputBox" type="text" onChange={e => setstatus(e.target.value)} />
                 </div>
-
+                <Multiselect 
+                    options={options} // Options to display in the dropdown
+                    onSelect={handleSelect} // Function will trigger on select event
+                    onRemove={handleRemove} // Function will trigger on remove event
+                    displayValue="text"
+                />
                 <div>
                     <button className="submitButton" type="submit">Submit Task</button>
                 </div>
