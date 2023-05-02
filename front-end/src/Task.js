9834b1b81f8ae424eaa165e292362425410f7db4
dc4472 <dc4472@nyu.edu>
2023-05-02 16:20:56 -0400

    Delete Plus 1

--- front-end/src/Task.js
+++ front-end/src/Task.js
@@ -18,7 +18,6 @@ const Task = props => {
 
     const { title, dueDate, status, badges } = props.task
     const dueDateFmt = new Date(dueDate)
-    dueDateFmt.setDate(dueDateFmt.getDate() + 1)
     const today = new Date()
 
     const checkDate = date => (date < today ? 'overdue' : '')
